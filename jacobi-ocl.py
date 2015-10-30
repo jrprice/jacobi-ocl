9e5cd2f777bc64ea2f08ba39297e8427066026bc
James Price <j.price@bristol.ac.uk>
2015-10-30 19:25:21 +0000

    Added arguments to control matrix size and iteration count.

--- jacobi-ocl.py
+++ jacobi-ocl.py
@@ -1,12 +1,16 @@
+import argparse
 import math
 import numpy
 import pyopencl as CL
 import time
 
-N       = 512
-ITRS    = 2000
-print 'N    = %d' % N
-print 'ITRS = %d' % ITRS
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--norder', type=int, default=256)
+parser.add_argument('-i', '--iterations', type=int, default=1000)
+args = parser.parse_args()
+
+print 'MATRIX     = %dx%d ' % (args.norder,args.norder)
+print 'ITERATIONS = %d' % args.iterations
 
 # Initialize OpenCL objects
 context = CL.create_some_context()
@@ -16,34 +20,36 @@ program = CL.Program(context, open('kernel.cl').read()).build()
 kernel  = program.jacobi
 
 # Create buffers
-d_A     = CL.Buffer(context, CL.mem_flags.READ_ONLY,  size=N*N*8)
-d_b     = CL.Buffer(context, CL.mem_flags.READ_ONLY,  size=N*8)
-d_x0    = CL.Buffer(context, CL.mem_flags.READ_WRITE, size=N*8)
-d_x1    = CL.Buffer(context, CL.mem_flags.READ_WRITE, size=N*8)
+vectorsize = args.norder*numpy.dtype(numpy.float64).itemsize
+matrixsize = args.norder*vectorsize
+d_A     = CL.Buffer(context, CL.mem_flags.READ_ONLY,  size=matrixsize)
+d_b     = CL.Buffer(context, CL.mem_flags.READ_ONLY,  size=vectorsize)
+d_x0    = CL.Buffer(context, CL.mem_flags.READ_WRITE, size=vectorsize)
+d_x1    = CL.Buffer(context, CL.mem_flags.READ_WRITE, size=vectorsize)
 d_xold  = d_x0
 d_xnew  = d_x1
 
 # Initialize data
 numpy.random.seed(0)
-h_A     = numpy.random.rand(N, N).astype(numpy.float64)
-for row in range(N):
+h_A     = numpy.random.rand(args.norder, args.norder).astype(numpy.float64)
+for row in range(args.norder):
     h_A[row][row] += numpy.sum(h_A[row])
-h_b     = numpy.random.rand(N).astype(numpy.float64)
-h_x     = numpy.zeros(N).astype(numpy.float64)
+h_b     = numpy.random.rand(args.norder).astype(numpy.float64)
+h_x     = numpy.zeros(args.norder).astype(numpy.float64)
 CL.enqueue_copy(queue, d_A, h_A)
 CL.enqueue_copy(queue, d_b, h_b)
 CL.enqueue_copy(queue, d_xold, h_x)
 
 kernel.set_arg(0, d_A)
 kernel.set_arg(1, d_b)
-kernel.set_arg(4, numpy.uint32(N))
+kernel.set_arg(4, numpy.uint32(args.norder))
 
 # Run Jacobi iterations
 start = time.time()
-for i in range(ITRS):
+for i in range(args.iterations):
     kernel.set_arg(2, d_xold)
     kernel.set_arg(3, d_xnew)
-    CL.enqueue_nd_range_kernel(queue, kernel, (N,), None)
+    CL.enqueue_nd_range_kernel(queue, kernel, (args.norder,), None)
 
     d_xold,d_xnew = d_xnew,d_xold
 
--- kernel.cl
+++ kernel.cl
@@ -2,15 +2,15 @@ kernel void jacobi(global double *A,
                    global double *b,
                    global double *xold,
                    global double *xnew,
-                   const unsigned Ndim)
+                   const unsigned N)
 {
   size_t i = get_global_id(0);
 
   double tmp = 0.0;
-  for (unsigned j = 0; j < Ndim; j++)
+  for (unsigned j = 0; j < N; j++)
   {
     if (i != j)
-      tmp += A[i*Ndim + j] * xold[j];
+      tmp += A[i*N + j] * xold[j];
   }
-  xnew[i] = (b[i] - tmp) / A[i*Ndim + i];
+  xnew[i] = (b[i] - tmp) / A[i*N + i];
 }
